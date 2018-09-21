2976785ccc80a6c49286757a003e5d10350bde19
Tony Batard <tbatard@pivotal.io>
2018-09-20 19:15:48 -0700

    Fixes failed test WelcomeControllerTest.welcomeMessage_showsMessageBasedOnProperty

--- src/test/java/com/example/helloconcourse/WelcomeControllerTest.java
+++ src/test/java/com/example/helloconcourse/WelcomeControllerTest.java
@@ -17,7 +17,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @TestPropertySource(properties = {
-        "message.welcome=Hello Tests",
+        "message.welcome=Hello From Tests",
 })
 public class WelcomeControllerTest {
 
